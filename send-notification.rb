d40a36dbbb89c383c391d56935624350998a549a
Katherine Lynch <katherly@upenn.edu>
2017-03-17 12:28:16 -0400

    Added config for URL

--- send-notification.rb
+++ send-notification.rb
@@ -2,6 +2,14 @@
 
 require 'rest-client'
 require 'json'
+require 'yaml'
+
+def load_config
+  config = YAML.load_file('config.yml')
+  @notifications_url = config['config']['notifications_url']
+end
+
+load_config
 
 # RestClient.post "http://example.com/resource", {'x' => 1}.to_json, {content_type: :json, accept: :json}
 json_file = ARGV.shift
@@ -9,7 +17,7 @@ json_file = ARGV.shift
 raise "Can't find JSON file: #{json_file}" unless File.exists? json_file
 data = open(json_file).read
 
-response = RestClient.post "http://sims-dev.library.upenn.edu:8084/iiif/notifications", data, { content_type: :json, accept: :json }
+response = RestClient.post @notifications_url, data, { content_type: :json, accept: :json }
 
 puts "response code is: #{response.code}"
 puts "response body is: #{response.body}"
